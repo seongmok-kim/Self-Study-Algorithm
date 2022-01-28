4c862e01f5ec06385bf467f5f866e13267b93c9a
smk <ksm1538@naver.com>
2022-01-28 22:34:09 +0900

    주석 추

--- self-study-algorithm/src/baekjoon/dynamic_programming/Baekjoon_No_2579.java
+++ self-study-algorithm/src/baekjoon/dynamic_programming/Baekjoon_No_2579.java
@@ -18,12 +18,16 @@ public class Baekjoon_No_2579 {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
+		// 첫 번째 계단 설정 
 		d[1] = arr[1];
+		// 계단이 1개만 주어질 수도 있으니 조건문으로 처리 
 		if(N>1) {
 			d[2] = arr[1] + arr[2];
 		}
+		
+		// 점화식을 이용해 Bottom-Up방식 이용 
 		for(int i=3;i<=N;i++) {
-			d[i] = Math.max(d[i-3]+arr[i-1], d[i-2]) + arr[i];
+			d[i] = Math.max(d[i-3]+arr[i-1] + arr[i], d[i-2] + arr[i]);
 		}
 		
 		System.out.println(d[N]);
