5868ef76e9667ad421c41d25229d299aeafc956c
ksm15 <ksm1538@naver.com>
2022-03-11 23:07:50 +0900

    백준 11399번 풀이

--- self-study-algorithm/src/baekjoon/sorting/Baekjoon_No_11399.java
+++ self-study-algorithm/src/baekjoon/sorting/Baekjoon_No_11399.java
@@ -1,10 +1,45 @@
 package baekjoon.sorting;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Baekjoon_No_11399 {
 
-	public static void main(String[] args) {
+	public static int N;
+	
+	public static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 오름차순으로 정렬
+		Arrays.sort(arr);
+		
+		int result = 0;
+		for(int i=0; i<N; i++) {
+			int sum = 0;
+			
+			for(int j=0; j<=i; j++) {
+				sum+= arr[j];
+			}
+			result += sum;
+		}
+		
+		System.out.println(result);
 	}
 
 }
