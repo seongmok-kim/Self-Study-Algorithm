4f5c52af30dfcf95f2ae79e30bed96edda783be2
ksm15 <ksm1538@naver.com>
2022-03-12 10:15:57 +0900

    백준 11399번 풀이

--- self-study-algorithm/src/baekjoon/sorting/Baekjoon_No_11399.java
+++ self-study-algorithm/src/baekjoon/sorting/Baekjoon_No_11399.java
@@ -8,12 +8,16 @@ import java.util.StringTokenizer;
 
 public class Baekjoon_No_11399 {
 
+	// N : 사람의 수
 	public static int N;
 	
+	// arr : 각 사람마다 걸리는 시간을 나열한 배열
 	public static int[] arr;
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		/*** 입력받기(시작) ***/
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
@@ -26,9 +30,15 @@ public class Baekjoon_No_11399 {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		br.close();
+		/*** 입력받기(끝) ***/
+		
 		// 오름차순으로 정렬
 		Arrays.sort(arr);
 		
+		// 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값 구하기
+		// ex 1) 첫번째 사람은 자신이 필요한 시간만 기다리면 됨.
+		// ex 2) 두번째 사람은 앞사람과 자신이 필요한 시간만큼 기다려야 함.
 		int result = 0;
 		for(int i=0; i<N; i++) {
 			int sum = 0;
@@ -39,6 +49,7 @@ public class Baekjoon_No_11399 {
 			result += sum;
 		}
 		
+		// 결과 출력
 		System.out.println(result);
 	}
 
