a99b3e71e7458ab5bd71618f9eb799609b4f5b55
sm <ksm1538@naver.com>
2022-03-20 18:42:34 +0900

    프로그래머스 - k진수에서 소수 개수 구하기

--- self-study-algorithm/src/programmers/get_prime_number_from_k.java
+++ self-study-algorithm/src/programmers/get_prime_number_from_k.java
@@ -24,6 +24,7 @@ public class get_prime_number_from_k {
 		
 		String changeNum = "";
 		
+		// k진수로 변환
 		while(n > 0) {
 			changeNum = (n%k) + changeNum;
 			n = n/k;
@@ -33,23 +34,29 @@ public class get_prime_number_from_k {
 		
 		String num = "";
 		for(int i=0; i<numArr.length; i++) {
+			// 0 을 만난 경우, 이전에 만났던 숫자 확인
 			if(numArr[i].equals("0")) {
+				// 이전에 만났던 숫자가 없으면 무시
 				if(num.equals("")) {
 					continue;
 				}
+				
+				// 이전에 만났던 숫자가 소수인지 체크 후
 				boolean check = isPrime(Long.parseLong(num));
 				
+				// 소수라면 카운트
 				if(check) {
 					answer++;
 				}
 				
 				num = "";
 			}
+			// 0 이 아니라면, 숫자 저장
 			else {
 				num += numArr[i];
 			}
 		}
-		
+		// 끝으로 저장된 숫자가 있을 수 있으니 한번 더 체크
 		if(!num.equals("")) {
 			boolean check = isPrime(Long.parseLong(num));
 			
