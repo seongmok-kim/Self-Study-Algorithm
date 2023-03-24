103c887c67de58de934117cb24081260fd1385de
ksm15 <ksm1538@naver.com>
2023-03-25 02:26:57 +0900

    백준 11057번 풀이

---
+++ self-study-algorithm/src/baekjoon/graph/Baekjoon_No_11507.java
@@ -0,0 +1,30 @@
+package baekjoon.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_No_11507 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int dp[][] = new int[N+1][10];
+        for(int i = 0; i < 10; i++) {
+            dp[0][i] = 1;
+        }
+
+        // 점화식 : dp[i][j] += dp[i-1][k];
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<10; j++){
+                for(int k=j; k<10; k++){
+                    dp[i][j] += dp[i-1][k];
+                    dp[i][j] %= 10007;
+                }
+            }
+        }
+
+        System.out.println(dp[N][0]);
+    }
+}
